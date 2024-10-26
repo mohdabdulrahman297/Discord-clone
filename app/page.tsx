f138c437a4f0aa7ec2f0c7e50e53e9ccaea338bb
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-10-26 03:37:35 -0500

    final test of git controls

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { Ghost } from "lucide-react";
 export default function Home() {
   return (
     <div className="text-3xl font-bold tect-indigo-500">
-      First code of this day and updates , changes made to git control
-       
+      final check
+
       <Button variant="ghost">
         click me
       </Button>
