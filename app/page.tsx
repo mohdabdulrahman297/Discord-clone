dbffae20ce9594f973063081602df8f88ea28e0b
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-10-26 03:35:53 -0500

    repo check

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { Ghost } from "lucide-react";
 export default function Home() {
   return (
     <div className="text-3xl font-bold tect-indigo-500">
-      First code of this day and updates 
+      First code of this day and updates , changes made to git control
+       
       <Button variant="ghost">
         click me
       </Button>
